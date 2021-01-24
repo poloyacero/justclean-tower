d286a213464d7c98b663eadf78a1303f8ffda3d9
Paul Kerwin Acero <akosipoloyacero@gmail.com>
2021-01-24 17:59:30 +0400

    update on query parameter show-with-offices=true

--- api/middlewares/paginate.js
+++ api/middlewares/paginate.js
@@ -1,11 +1,32 @@
+const db = require('../database/mysql');
 const { Op } = require("sequelize");
+const Offices = db.offices;
+
 filter = (model) => {
     return async (req, res, next) => {
         try{
             const page = parseInt(req.query.page);
             const limit = parseInt(req.query.limit);
+            const show_with_offices = req.query['show-with-offices'];
             const { location, num_floors, num_offices, rating, sort, order } = req.query;
             const ordering = [];
+            const includes = [];
+ 
+            if(show_with_offices && show_with_offices !== undefined) {
+                includes.push({
+                    model: Offices,
+                    as: 'offices',
+                    required: true
+                });
+            }else{
+                includes.push({
+                    model: Offices,
+                    as: 'offices',
+                    required: false
+                });
+            }
+
+            console.log('cludes', includes[0]);
 
             if(sort !== null && order !== undefined) {
                 ordering.push(sort, order)
@@ -14,7 +35,7 @@ filter = (model) => {
             }else{
                 ordering.push('id');
             }
-
+            console.log(ordering);
             const startIndex = (page - 1) * limit;
             const endIndex = page * limit;
 
@@ -46,6 +67,9 @@ filter = (model) => {
                             }
                         ]
                     },
+                    include: [
+                        includes[0]
+                    ],
                     order: [
                         ordering
                     ],
@@ -93,6 +117,9 @@ filter = (model) => {
                             }
                         ]
                     },
+                    include: [
+                        includes[0]
+                    ],
                     order: [
                         ordering
                     ]
